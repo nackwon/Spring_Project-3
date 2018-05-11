98692ab1f1734a16a2a3c5a2d0d26e2198765e1b
ahwk3 <ahwk3@DESKTOP-AHK45VR>
2018-05-11 13:56:02 +0900

    All SQL

---
+++ webapp/WEB-INF/jblog.sql
@@ -0,0 +1,156 @@
+/* users Table Start */
+CREATE TABLE users(
+	userNo NUMBER PRIMARY KEY,
+	id VARCHAR2(50) UNIQUE NOT NULL,
+	userName VARCHAR2(100) NOT NULL,
+	password VARCHAR2(50) NOT NULL,
+	joinDate DATE NOT NULL
+);
+
+CREATE SEQUENCE seq_users_no
+INCREMENT BY 1
+START WITH 1
+NOCACHE;
+
+SELECT id FROM users;
+
+DELETE FROM users;
+DROP TABLE users;
+DROP SEQUENCE seq_users_no;
+/* users Table End*/
+---------------------------------------
+/* blog Table Start */
+CREATE TABLE blog(
+	id VARCHAR2(50) PRIMARY KEY,
+	blogTitle VARCHAR2(200) NOT NULL,
+	logoFile VARCHAR2(200),
+	CONSTRAINT u_blog_fk FOREIGN KEY (id)
+	REFERENCES users(id)
+);
+
+SELECT A.id, A.blogTitle, A.logoFile, c.cateName
+FROM (SELECT u.id, b.blogTitle, b.logoFile
+	  FROM blog b, users u
+      WHERE b.id = u.id) A, category c
+WHERE c.id = A.id;
+
+DELETE FROM blog;
+SELECT id FROM blog;
+DROP TABLE blog;
+
+/* blog Table End*/
+---------------------------------------
+/* category Table Start*/
+CREATE TABLE category(
+	cateNo NUMBER PRIMARY KEY,
+	id VARCHAR2(50),
+	cateName VARCHAR2(200) NOT NULL,
+	description VARCHAR2(500),
+	regDate DATE NOT NULL,
+	CONSTRAINT u_category_fk FOREIGN KEY (id)
+	REFERENCES users(id)
+);
+
+CREATE SEQUENCE seq_category_no
+INCREMENT BY 1
+START WITH 1
+NOCACHE;
+
+SELECT cateName, regDate
+FROM category
+WHERE cateNo = 
+
+SELECT cateNo
+	   ,id
+	   ,cateName
+	   ,description
+	   ,regDate
+	   ,(SELECT count(postNo)
+		FROM post
+		WHERE category.cateNo = post.cateNo
+		) postCount 
+FROM category
+WHERE cateNo = 4;
+
+SELECT cateNo
+	  ,cateName
+	  ,description
+	  ,(SELECT count(postNo)
+		FROM post
+		WHERE category.cateNo = post.cateNo
+		) postCount
+FROM category
+WHERE id = 'jimmy'
+ORDER BY cateNo DESC;
+
+SELECT * FROM CATEGORY; 
+DELETE FROM category;
+DROP TABLE category;
+DROP SEQUENCE seq_category_no;
+
+SELECT * FROM CATEGORY;
+/* category Table End */
+---------------------------------------
+/* post Table Start */
+CREATE TABLE post(
+	postNo NUMBER PRIMARY KEY,
+	cateNo NUMBER,
+	postTitle VARCHAR2(300) NOT NULL,
+	postContent VARCHAR2(4000) NOT NULL,
+	regDate DATE NOT NULL,
+	CONSTRAINT c_post_fk FOREIGN KEY (cateNo)
+	REFERENCES category(cateNo)
+);
+
+SELECT postTitle, regDate
+FROM post
+WHERE cateNo = 5;
+
+SELECT count(*)
+FROM post p, category c
+WHERE c.cateNo = p.cateNo
+AND p.cateNo = 6;
+
+CREATE SEQUENCE seq_post_no
+INCREMENT BY 1
+START WITH 1
+NOCACHE;
+
+SELECT * FROM post;
+
+DELETE FROM post;
+DROP TABLE post;
+DROP SEQUENCE seq_post_no;
+/* post Table End */
+---------------------------------------
+/* comments Table Start */
+CREATE TABLE comments(
+	cmtNo NUMBER PRIMARY KEY,
+	postNo NUMBER,
+	userNo NUMBER,
+	cmtContent VARCHAR2(1000) NOT NULL,
+	regDate DATE NOT NULL,
+	CONSTRAINT p_comments_fk FOREIGN KEY (postNO)
+	REFERENCES post(postNo),
+	CONSTRAINT u_comments_fk FOREIGN KEY (userNo)
+	REFERENCES users(userNo)
+);
+
+CREATE SEQUENCE seq_comments_no
+INCREMENT BY 1
+START WITH 1
+NOCACHE;
+
+DROP TABLE comments;
+DROP SEQUENCE seq_comments_no;
+/* comments Table End */
+
+
+
+COMMIT
+
+
+
+
+
+
